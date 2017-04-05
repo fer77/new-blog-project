705f8b5e2f60a18226c6eae3c18ee62f92c28384
Fernando Fernandez <ffmp777@yahoo.com>
2017-04-05 07:55:32 -0600

    refactored the 'archives' method, added to Post.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,9 +11,17 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
+     //* After all service providers have been registered Laravel will filter again and call boot.
+     //* THis is a place where you can perform any action or logic with the assumption is already loaded.
     public function boot()
     {
-        //
+        //* Register a view composer, with a view facade.
+        //* we want "archives" in our blog to be visible everywhere this layout is shown.
+        view()->composer('layouts.blog-sidebar', function($view) {
+          //* We can hook up to when any view is loaded.
+          //* The call back function binds $view to the variable 'archives'.
+          $view->with('archives', \App\Post::archives());
+        }); //* Now this view will have access to a collection of all posts.
     }
 
     /**
@@ -21,6 +29,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
+     //* We can bind things itno the service container.
     public function register()
     {
         //
