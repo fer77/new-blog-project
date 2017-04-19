ae40765a52b8c6d5cb74806866ce185dd4de136e
Fernando Fernandez <ffmp777@yahoo.com>
2017-04-19 08:22:54 -0600

    created an event and two listeners to handle notifications

---
+++ app/Listeners/NotifySubscribers.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ThreadCreated;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class NotifySubscribers
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  ThreadCreated  $event
+     * @return void
+     */
+    public function handle(ThreadCreated $event)
+    {
+        var_dump($event->thread['name'] . ' was published to the forum.');
+    }
+}
+//* To test we can run:
+//* php artisan tinker
+//* event(new App\Events\ThreadCreated(['name'=>"Some new thread"]));
